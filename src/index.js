ba88d588ea8a319953bac0ab1ae803eb7e46adab
isacpxc <isacpxc@gmail.com>
2022-05-04 10:20:27 -0300

    route fix it paths of new.js 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const router = express.Router();
+const newedital = require("../routes/edital/new");
 
 const app = express();
 const port = 8080;
+app.use(express.json())
+
+app.use('/new', newedital);
+
 app.listen(port, () => {
     console.log(`Servindo em http://localhost:${port}`);
 });
+
