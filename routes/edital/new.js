d468cf2f6de6e8ce95e89e2425259b128cf9ddf9
isacpxc <isacpxc@gmail.com>
2022-05-14 12:33:48 -0300

    route get new\edital receiving data from query | no authentication yet

--- routes/edital/new.js
+++ routes/edital/new.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-
+//receive data to create edital
 router.get('/edital', (req, res)=>{
-    res.json(req.query.testEnter)
+    const infoForm = {             
+        nameEd: req.query.nameEd,       // edital name
+        sNameEd: req.query.sNameEd,     // short name edital
+        dStartSub: req.query.dStartSub, // beginning date of submissions
+        dEndSub: req.query.dEndSub,    // end date of submissions
+        dStartRev: req.query.dStartRev, // beginning date of review/assesment
+        dEndRev: req.query.dEndRev,     // end date of review/assesment
+        dStartEv: req.query.dStartEv,   // beginning date event
+        dEndEv: req.query.dEndEv,       // end date event
+        logo: req.query.logo,           // logo.png
+        catSheet: req.query.catSheet,   // catalog sheet
+    }
+    res.json(infoForm);
 })
 
 module.exports = router;
